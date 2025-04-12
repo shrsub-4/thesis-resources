52dfde4e0e77e07fdc6480ad359a2b461d56aed6
Shrijan <shrsub-4@student.ltu.se>
2025-04-12 20:58:59 +0100

    changed inference apps

--- inference_service/client/client.py
+++ inference_service/client/client.py
@@ -1,28 +1,34 @@
 import requests
+import time
 
-# Define the server URL
-url = "http://10.104.0.94:31245/upload"
+def sendRequest():
+    # Define the server URL
+    url = "http://10.104.8.215:31544/upload"
 
-# Set the headers (including the required Host header)
-headers = {
-    "Host": "gateway.default.example.com"
-}
+    # Set the headers (including the required Host header)
+    headers = {
+        "Host": "appservice.default.example.com"
+    }
 
-# Define the form data
-data = {
-    "lidar_distance": "2.0",
-    "radar_speed": "5.4",
-    "ultrasonic": "3.2"
-}
+    # Define the form data
+    data = {
+        "lidar_distance": "2.0",
+        "radar_speed": "5.4",
+        "ultrasonic": "3.2"
+    }
 
-# Define the file to upload
-files = {
-    "image": ("img.webp", open("img.webp", "rb"), "image/webp")
-}
+    # Define the file to upload
+    files = {
+        "image": ("img.webp", open("img.webp", "rb"), "image/webp")
+    }
+    start_time = time.time()
+    response = requests.post(url, headers=headers, data=data, files=files)
+    duration = time.time() - start_time
+    print(f"Response_time ======== {duration}")
+    print("Response Code:", response.status_code)
+    print("Response Body:", response.text)
 
-# Send the POST request
-response = requests.post(url, headers=headers, data=data, files=files)
-
-    # Print the server's response
-print("Response Code:", response.status_code)
-print("Response Body:", response.text)
+if __name__=="__main__":
+    for i in range(100):
+        sendRequest()
+        time.sleep(1)
